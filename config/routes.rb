06e0d4d8e40739050d3974cc3b8b3b4fb3da1442
Elgert <gerti_g@hotmail.com>
2022-02-11 12:13:13 +0100

    initial resource creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :talents
+  resources :authors
+  resources :courses
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
