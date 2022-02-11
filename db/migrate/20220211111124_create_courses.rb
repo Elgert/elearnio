99427270348c1d5b2d06964b2e8ed3ec32d56ca5
Elgert <gerti_g@hotmail.com>
2022-02-11 14:20:59 +0100

    added jsonapi gem and config

--- db/migrate/20220211111124_create_courses.rb
+++ db/migrate/20220211111124_create_courses.rb
@@ -1,6 +1,7 @@
 class CreateCourses < ActiveRecord::Migration[6.0]
   def change
     create_table :courses, id: :serial do |t|
+      t.integer :author_id, null: false
       t.string :name, null: false
       t.string :description
       t.string :category
